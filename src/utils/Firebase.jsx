864c598778e4c4f715b062c8c323c2ef2831d527
Hansal <hansaltamrakar@gmail.com>
2023-11-09 14:02:29 +0530

    done

--- src/utils/Firebase.jsx
+++ src/utils/Firebase.jsx
@@ -8,14 +8,23 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDiU2l7t9oXlZoEspZS5deLZvr4MxI6akU",
+//   authDomain: "netflixapp-9b1bc.firebaseapp.com",
+//   projectId: "netflixapp-9b1bc",
+//   storageBucket: "netflixapp-9b1bc.appspot.com",
+//   messagingSenderId: "586861069352",
+//   appId: "1:586861069352:web:e2f222c331fc07b967a11d",
+//   measurementId: "G-1NSLJXXD3P",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDiU2l7t9oXlZoEspZS5deLZvr4MxI6akU",
-  authDomain: "netflixapp-9b1bc.firebaseapp.com",
-  projectId: "netflixapp-9b1bc",
-  storageBucket: "netflixapp-9b1bc.appspot.com",
-  messagingSenderId: "586861069352",
-  appId: "1:586861069352:web:e2f222c331fc07b967a11d",
-  measurementId: "G-1NSLJXXD3P",
+  apiKey: "AIzaSyBED8UKuDfwRReflN3rGas2fou1D3U7dbI",
+  authDomain: "netflix-147df.firebaseapp.com",
+  projectId: "netflix-147df",
+  storageBucket: "netflix-147df.appspot.com",
+  messagingSenderId: "36315369436",
+  appId: "1:36315369436:web:6bdc084aaf2ef8f54b0919",
+  measurementId: "G-FSS6SHC3E1",
 };
 
 // Initialize Firebase
